9ff73f693cedf8398d356bb1e42c7eba60d8be56
oryam <16582185+oryam@users.noreply.github.com>
2018-09-23 06:00:27 +0200

    add export path map on posts, components, login pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-// const path = require('path');
-// const webpack = require('webpack');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
-// const withOptimizedImages = require('next-optimized-images');
 const isProd = process.env.NODE_ENV === 'production';
-module.exports = withCSS(withSass(withImages({
+module.exports = withCSS(
+  withSass(
+    withImages({
       cssModules: true,
       serverRuntimeConfig: {},
       publicRuntimeConfig: {
         // Will be available on both server and client
-        // appContext: isProd ? '/hello-next/' : '/', // for gh-pages
+        // appContext: isProd ? '/hello-next/' : '/' // for gh-pages
         appContext: '/',
       },
       // assetPrefix: isProd ? '/hello-next' : '', // for gh-pages
@@ -18,32 +17,13 @@ module.exports = withCSS(withSass(withImages({
         return {
           '/': { page: '/' },
           '/about': { page: '/about' },
-          // '/posts': { page: '/posts' },
-          // '/login': { page: '/views/LoginPage' },
+          '/posts': { page: '/posts' },
           '/p/975': { page: '/post', query: { id: 975 } },
-          '/p/504': { page: '/post', query: { id: 504 } }
+          '/p/504': { page: '/post', query: { id: 504 } },
+          '/login': { page: '/login' },
+          '/components': { page: '/components' }
         };
-      },
-      // webpack: (config, { dev, isServer }) => {
-      //   // for (let plugin of config.plugins) {
-      //   //   console.log('plugin', plugin.constructor.name);
-      //   // }
-      //   if (!isServer) {
-      //     config.plugins.push(
-      //       new webpack.optimize.AggressiveSplittingPlugin({
-      //         // minSize: 30000,
-      //         maxSize: 50000
-      //       })
-      //       // new webpack.DefinePlugin({
-      //       //   'process.env.NODE_ENV': JSON.stringify('production')
-      //       // })
-      //     );
-      //   }
-
-      //   config.recordsOutputPath = path.join(__dirname, "out", "records.json");
-
-      //   return config;
-      // }
+      }
     })
   )
 );
